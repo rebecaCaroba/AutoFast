fdf5961a67cd5aca820299dc275cd02988d01128
Rebeca Caroba <rebecacaroba@gmail.com>
2026-08-31 19:48:39 -0300

    Incorporando componente Chat ao clicar na Tab chat

---
+++ src/components/Chat/index.tsx
@@ -0,0 +1,9 @@
+import './style.scss'
+
+export function Chat() {
+    return (
+        <div className="chat">
+            teste
+        </div>
+    )
+}
--- src/components/ModalBudget/index.tsx
+++ src/components/ModalBudget/index.tsx
@@ -1,6 +1,7 @@
 import { IoMdClose } from "react-icons/io";
 import './style.scss'
 import { useState } from "react";
+import { Chat } from "../Chat";
 
 interface ModalBudgetProps {
     toggleModalBudget: () => void
@@ -13,6 +14,7 @@ export function ModalBudget({ toggleModalBudget }: ModalBudgetProps) {
     const [isActiveNav, setIsActiveNav] = useState<'bud' | 'chat'>('bud')
 
     return (
+
         <div className="modal-budget">
             <div className='modal-budget-container'>
                 <div className="modal-budget-header">
@@ -31,58 +33,67 @@ export function ModalBudget({ toggleModalBudget }: ModalBudgetProps) {
                     </div>
 
                     <nav className="modal-budget-header-nav">
-                        <li 
-                        onClick={() => setIsActiveNav('bud')} 
-                        className={`${isActiveNav ==  'bud' ? 'active' : ''}`}>
+                        <li
+                            onClick={() => setIsActiveNav('bud')}
+                            className={`${isActiveNav == 'bud' ? 'active' : ''}`}>
                             Orçamento
                         </li>
-                        <li 
-                        onClick={() => setIsActiveNav('chat')}
-                        className={`${isActiveNav == 'chat' ? 'active' : ''}`}>
+                        <li
+                            onClick={() => setIsActiveNav('chat')}
+                            className={`${isActiveNav == 'chat' ? 'active' : ''}`}>
                             Chat
                         </li>
                     </nav>
                 </div>
-                <div className='modal-budget-body'>
-                    <div className='modal-budget-body-info'>
-                        <p><strong>Cliente:</strong> João Vitor</p>
-                        <p><strong>Telefone:</strong> (99) 99999-9999</p>
-                        <p><strong>Veículo:</strong> Honda Civic</p>
-                        <p><strong>Peça:</strong> Eixo Dianteiro</p>
-                    </div>
-                    <div className="modal-budget-body-img">
-                        <img src="https://picsum.photos/seed/picsum/200" alt="Foto da peça solicitada" />
-                    </div>
-
-                    <details>
-                        <summary>Descrição do problema</summary>
-                        <div className="summary-desc">
-                            <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        </div>
-                    </details>
-
-                    <details open>
-                        <summary>Preço e prazo</summary>
-                        <div className='summary-price'>
-                            <div className="price-left">
-                                <p>Valor do orçamento</p>
-                                <strong>R$432,00</strong>
+                {isActiveNav === 'bud' ? (
+                    <>
+                        <div className='modal-budget-body'>
+                            <div className='modal-budget-body-info'>
+                                <p><strong>Cliente:</strong> João Vitor</p>
+                                <p><strong>Telefone:</strong> (99) 99999-9999</p>
+                                <p><strong>Veículo:</strong> Honda Civic</p>
+                                <p><strong>Peça:</strong> Eixo Dianteiro</p>
                             </div>
-                            <div className="price-right">
-                                <p>Prazo de entrega</p>
-                                <strong>5 dias utéis</strong>
+                            <div className="modal-budget-body-img">
+                                <img src="https://picsum.photos/seed/picsum/200" alt="Foto da peça solicitada" />
                             </div>
+
+                            <details>
+                                <summary>Descrição do problema</summary>
+                                <div className="summary-desc">
+                                    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                </div>
+                            </details>
+
+                            <details open>
+                                <summary>Preço e prazo</summary>
+                                <div className='summary-price'>
+                                    <div className="price-left">
+                                        <p>Valor do orçamento</p>
+                                        <strong>R$432,00</strong>
+                                    </div>
+                                    <div className="price-right">
+                                        <p>Prazo de entrega</p>
+                                        <strong>5 dias utéis</strong>
+                                    </div>
+                                </div>
+                            </details>
                         </div>
-                    </details>
-                </div>
-                {budgetStatus === 'new' ? (
-                    <div className='modal-budget-footer'>
-                        <button type="button" className='modal-budget-btn-reject'>Recusar</button>
-                        <button type="button" className='modal-budget-btn-approve'>Aprovar orçamento</button>
-                    </div>
+                        {budgetStatus === 'new' ? (
+                            <div className='modal-budget-footer'>
+                                <button type="button" className='modal-budget-btn-reject'>Recusar</button>
+                                <button type="button" className='modal-budget-btn-approve'>Aprovar orçamento</button>
+                            </div>
+                        ) : (
+                            ''
+                        )}
+                    </>
                 ) : (
-                    ''
-                )}
+                    <Chat />
+                )
+
+                }
+
             </div>
         </div>
     )
